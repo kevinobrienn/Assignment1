7ab082e08b487aa934108c75ee0c0d815354b2bb
Kevin Obrien <krobrie5@asu.edu>
2020-10-03 13:53:35 -0700

    Initial commit of Adding Machine.

---
+++ Assignment1/src/cse360assignment01/AddingMachine.java
@@ -0,0 +1,46 @@
+package cse360assignment01;
+
+public class AddingMachine {
+	private int total;
+	  
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+  
+	/*
+	* The getTotal method implements returns the current total
+	* of the Adding machine class.
+	*/
+	public int getTotal () {
+		return 0;
+	}
+	
+	/*
+	* The add method adds the parameter to the total variable
+	* of the Adding machine class.
+	*/
+	public void add (int value) {
+	}
+	
+	/*
+	* The subtract method subtracts the parameter from the total 
+	* variable of the Adding machine class.
+	*/
+	public void subtract (int value) {
+	}
+
+	/*
+	* The toString method keeps a history of the transactions 
+	* and returns the history as a string.
+	*/
+	public String toString () {
+		return "";
+	}
+	
+	/*
+	* The clear method deletes the history of transactions
+	* and resets the total to 0
+	*/
+	public void clear() {
+	}
+}
