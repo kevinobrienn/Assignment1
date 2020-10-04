02d668cf254ea208e3447ec90ead9ea1a2a5a1ac
Kevin Obrien <krobrie5@asu.edu>
2020-10-04 14:13:42 -0700

    Added a heading, main method, history variable, and completed the getTotal, add, subtract, toString, and clear methods.

--- Assignment1/src/cse360assignment01/AddingMachine.java
+++ Assignment1/src/cse360assignment01/AddingMachine.java
@@ -1,9 +1,35 @@
 package cse360assignment01;
 
+//Assignment #: 1 
+//Arizona State University - CSE360
+//      Name: Kevin OBrien
+// StudentID: 1214786131
+//   Lecture: T 9:00 
+//Description: Performs predefined math actions and displays each 
+//				 action to the user.
+
 public class AddingMachine {
 	private int total;
-	  
-	public AddingMachine () {
+	private String history = "0 ";
+
+	/*
+	* The main method of the AddingMachine class is the
+	* driver for the program. 
+	*/
+	public static void main(String[] args) {
+		AddingMachine myCalculator = new AddingMachine();
+		myCalculator.add(4); 
+		myCalculator.subtract(2); 
+		myCalculator.add(5);
+		System.out.println(myCalculator.toString());
+		return;
+	}
+	
+	/*
+	* The constructor for the Adding machine class.
+	* Sets total to 0.
+	*/
+	public AddingMachine() {
 		total = 0;  // not needed - included for clarity
 	}
   
@@ -11,30 +37,34 @@ public class AddingMachine {
 	* The getTotal method implements returns the current total
 	* of the Adding machine class.
 	*/
-	public int getTotal () {
-		return 0;
+	public int getTotal() {
+		return total;
 	}
 	
 	/*
 	* The add method adds the parameter to the total variable
 	* of the Adding machine class.
 	*/
-	public void add (int value) {
+	public void add(int value) {
+		total += value;
+		history += "+ " + value + " ";
 	}
 	
 	/*
 	* The subtract method subtracts the parameter from the total 
 	* variable of the Adding machine class.
 	*/
-	public void subtract (int value) {
+	public void subtract(int value) {
+		total -= value; 
+		history += "- " + value + " ";
 	}
 
 	/*
 	* The toString method keeps a history of the transactions 
 	* and returns the history as a string.
 	*/
-	public String toString () {
-		return "";
+	public String toString() {
+		return history;
 	}
 	
 	/*
@@ -42,5 +72,7 @@ public class AddingMachine {
 	* and resets the total to 0
 	*/
 	public void clear() {
+		total = 0;
+		history = "0 ";
 	}
 }
